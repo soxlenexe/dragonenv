0a78806d83d03c81eda2f3c1b4aad17bfbb2fb3c
soxlenexe <70555800+soxlenexe@users.noreply.github.com>
2021-03-01 14:03:31 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='')
+ 
+@app.route("/")
+def main():
+    return render_template('index.html')
+ 
+if __name__ == "__main__":
+    app.run(port=3000,debug=True)
